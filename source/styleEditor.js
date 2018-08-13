980a401805f648e7d98bbcebf75b43a7bf4b8ee5
Jliv316 <jclivingston316@gmail.com>
2018-08-13 07:35:46 -0600

    first commit

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  // your code goes here.
+
+  editStyle = (selector, property, value) => {
+    $(selector).css(property, value);
+  }
+
+  $("input[type=submit]").on("click", () => {
+
+    const selector = $(style_editor[name = "selector"]).val();
+    const property = $(style_editor[name = "property"]).val();
+    const value = $(style_editor[name = "value"]).val();
+
+    editStyle(selector, property, value);
+    event.preventDefault();
+  });
 });
